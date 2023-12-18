07254262e93d31493d6f9e66e5f5b548c90dfc16
eveeify <eveeg1971@gmail.com>
2023-12-18 18:01:17 +1100

    chore: seo!

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Figtree } from "next/font/google";
 import "./globals.css";
 import Sidebar from "@/components/Sidebar";
@@ -7,12 +7,20 @@ import UserProvider from "./providers/UserProvider";
 import ModelProvider from "./providers/ModelProvider";
 import ToasterProvider from "./providers/ToasterProvider";
 import getSongsByUserId from "@/actions/getSongsByUserId";
+import SEOImage from "./assets/SEO.jpeg";
 
 const font = Figtree({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Spotify Clone",
-  description: "Music Streaming Clone by Eveeify",
+  description: "Music Steaming clone by eveeify",
+  icons: {
+    icon: SEOImage.src,
+  },
+};
+
+export const viewport: Viewport = {
+  themeColor: "#10b981",
 };
 
 export const revalidate = 0;
@@ -25,7 +33,6 @@ export default async function RootLayout({
   const userSongs = await getSongsByUserId();
   return (
     <html lang="en">
-      <meta content="#10b981" data-react-helmet="true" name="theme-color" />
       <body className={font.className}>
         <ToasterProvider />
         <SupabaseProvider>
